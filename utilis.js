8204a276d77c9ccae72014de18f69def5b4734b5
anthonyzNFT <azuniga.pro@gmail.com>
2025-09-25 01:22:26 -0700

    Create utilis.js

---
+++ utilis.js
@@ -0,0 +1,13 @@
+export function shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+}
+
+export function generateUUID() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, c => {
+        const r = Math.random() * 16 | 0;
+        return (c === 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+    });
+}
